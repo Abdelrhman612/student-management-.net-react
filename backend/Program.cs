16a411f6684047448fb6facede61fac561384a75
Abdelrhman <abdelrahman.ayman.tech@gmail.com>
2025-11-08 11:04:40 +0200

    update Program.cs and student-management.csproj

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,13 +1,36 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using student_management.data;
+using student_management.Authentication;
 using student_management.InterFaces;
 using student_management.Services;
+using student_management.Authentication.InterFace;
+using student_management.Authentication.Service;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<AppDbContext>(ob => ob.UseSqlServer(builder.Configuration.GetConnectionString("myCon")));
+var jwtOptions = builder.Configuration.GetSection("JWT").Get<Jwt>();
+builder.Services.AddAuthentication().AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+{
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidIssuer = jwtOptions.Issuer,
+        ValidateAudience = true,
+        ValidAudience = jwtOptions.Audience,
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOptions.key)),
+
+    };
+});
 builder.Services.AddScoped<IStudentService, StudentService>();
+builder.Services.AddScoped<IAuthService, AuthService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
