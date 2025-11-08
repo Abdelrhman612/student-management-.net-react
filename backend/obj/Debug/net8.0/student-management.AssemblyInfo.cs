a9f89f8354d14df31a86d4352580ed2b88b13ddf
Abdelrhman <abdelrahman.ayman.tech@gmail.com>
2025-11-08 11:04:59 +0200

    update 8 files

--- backend/obj/Debug/net8.0/student-management.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/student-management.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("student-management")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80203dad7a27e2a65808a2ca6653e387d0ced01b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc941b3d314e8f8636406aaa9145758f4862d940")]
 [assembly: System.Reflection.AssemblyProductAttribute("student-management")]
 [assembly: System.Reflection.AssemblyTitleAttribute("student-management")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
