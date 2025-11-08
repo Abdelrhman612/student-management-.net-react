6b447ef5da3b53014a4d3a1c1098928c2d4bffdf
Abdelrhman <abdelrahman.ayman.tech@gmail.com>
2025-11-08 11:05:10 +0200

    feat: create AuthController.cs, AuthDto.cs and Jwt.cs

---
+++ backend/Authentication/Dto/AuthDto.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace student_management.Authentication.Dto
+{
+    public class SignUpDto
+    {
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+    }
+
+    public class SignInDto
+    {
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+
+    }
+
+
+}
