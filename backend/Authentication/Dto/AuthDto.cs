6dc1d5075f95685024d111a55deee56a2178c83c
Abdelrhman <abdelrahman.ayman.tech@gmail.com>
2025-11-11 05:59:53 +0200

    update AuthDto.cs

--- backend/Authentication/Dto/AuthDto.cs
+++ backend/Authentication/Dto/AuthDto.cs
@@ -18,6 +18,7 @@ namespace student_management.Authentication.Dto
     {
         public required string Email { get; set; }
         public required string Password { get; set; }
+        public string? Role { get; set; } = null;
 
     }
 
